657650dccf20abed7458e82335f5630bcdf0c2fa
James <86011275+James000916@users.noreply.github.com>
2023-11-08 12:44:52 +0800

    v1.1

--- background.js
+++ background.js
@@ -1,8 +1,38 @@
+const hideAds = async () => {
+  await new Promise((resolve, _reject) => {
+    const videoContainer = document.getElementById("movie_player");
+
+    const setTimeoutHandler = () => {
+      const isAd = videoContainer?.classList.contains("ad-interrupting") || videoContainer?.classList.contains("ad-showing");
+      const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
+      const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
+      const videoPlayer = document.getElementsByClassName("video-stream")[0];
+
+      if (isAd && skipLock) {
+        videoPlayer.muted = true;
+        videoPlayer.currentTime = videoPlayer.duration - 1;
+        videoPlayer.paused && videoPlayer.play()
+        document.querySelector(".ytp-ad-skip-button")?.click();
+        document.querySelector(".ytp-ad-skip-button-modern")?.click();
+      } else if (isAd && surveyLock) {
+        document.querySelector(".ytp-ad-skip-button")?.click();
+        document.querySelector(".ytp-ad-skip-button-modern")?.click();
+      }
+
+      resolve();
+    };
+
+    setTimeout(setTimeoutHandler, 100);
+  });
+
+  hideAds();
+};
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && String(tab.url).includes("https://www.youtube.com")) {
+  if (changeInfo.status === "complete" && String(tab.url).includes("https://www.youtube.com/watch")) {
     chrome.scripting.executeScript({
       target: { tabId: tabId },
-      files : [ "script.js" ],
+      function: hideAds,
     });
   }
 });
--- script.js
+++ script.js
@@ -1,56 +1,55 @@
-// Thank you for using MeowBlock!😺
+const hideAds = async () => {
+  await new Promise((resolve, _reject) => {
+    const videoContainer = document.getElementById("movie_player");
 
-var video = document.querySelector("video");
-if (video) {
-  video.addEventListener("timeupdate", function() {
-    skipAd();
-    removeStaticAds();
+    const setTimeoutHandler = () => {
+      const isAd = videoContainer?.classList.contains("ad-interrupting") || videoContainer?.classList.contains("ad-showing");
+      const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
+      const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
+      const videoPlayer = document.getElementsByClassName("video-stream")[0];
+
+      if (isAd && skipLock) {
+        videoPlayer.muted = true;
+        videoPlayer.currentTime = videoPlayer.duration - 1;
+        videoPlayer.paused && videoPlayer.play()
+        document.querySelector(".ytp-ad-skip-button")?.click();
+        document.querySelector(".ytp-ad-skip-button-modern")?.click();
+      } else if (isAd && surveyLock) {
+        document.querySelector(".ytp-ad-skip-button")?.click();
+        document.querySelector(".ytp-ad-skip-button-modern")?.click();
+      }
+
+      const staticAds = [
+        ".ytd-companion-slot-renderer", ".ytd-action-companion-ad-renderer", // in-feed video ads
+        ".ytd-watch-next-secondary-results-renderer.sparkles-light-cta", ".ytd-unlimited-offer-module-renderer", // similar components
+        ".ytp-ad-overlay-image", ".ytp-ad-text-overlay", // deprecated overlay ads
+        "div#root.style-scope.ytd-display-ad-renderer.yt-simple-endpoint", "div#sparkles-container.style-scope.ytd-promoted-sparkles-web-renderer",
+        ".ytd-display-ad-renderer", ".ytd-statement-banner-renderer", ".ytd-in-feed-ad-layout-renderer", // homepage ads
+        "div#player-ads.style-scope.ytd-watch-flexy, div#panels.style-scope.ytd-watch-flexy", // sponsors
+        ".ytd-banner-promo-renderer", ".ytd-video-masthead-ad-v3-renderer", ".ytd-primetime-promo-renderer", // subscribe for premium & youtube tv ads
+        "#masthead-ad", ".ytd-ad-slot-renderer", ".ytd-promoted-sparkles-web-renderer" // others
+      ];
+
+      staticAds.forEach((ad) => {
+          document.hideElementsBySelector(ad);
+      });
+
+      resolve();
+    };
+
+    setTimeout(setTimeoutHandler, 100);
   });
-}
-removeStaticAds();
 
-function skipAd() {
-  const videoContainer = document.getElementById("movie_player");
-  const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
-  const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
-  if (videoContainer) {
-    const isAd = videoContainer.classList.contains("ad-interrupting") || videoContainer.classList.contains("ad-showing");
-    if (isAd && skipLock) {
-      video.muted = true;
-      video.currentTime = video.duration - 1;
-      if (video.paused) {
-        video.play();
-      }
-      const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-skip-button-modern");
-      if (skipButton) {
-        skipButton.click();
-      }
-    } else if (isAd && surveyLock) {
-      const skipButton = document.querySelector(".ytp-ad-skip-button, .ytp-ad-skip-button-modern");
-      if (skipButton) {
-        skipButton.click();
-      }
-    }
-  }
+  hideAds();
 };
 
-function removeStaticAds() {
-  const pageAds = [
-    ".ytd-companion-slot-renderer", ".ytd-action-companion-ad-renderer", // in-feed video ads
-    ".ytd-watch-next-secondary-results-renderer.sparkles-light-cta", ".ytd-unlimited-offer-module-renderer", // similar components
-    ".ytp-ad-overlay-image", ".ytp-ad-text-overlay", // deprecated overlay ads
-    "div#root.style-scope.ytd-display-ad-renderer.yt-simple-endpoint", "div#sparkles-container.style-scope.ytd-promoted-sparkles-web-renderer",
-    ".ytd-display-ad-renderer", ".ytd-statement-banner-renderer", ".ytd-in-feed-ad-layout-renderer", // homepage ads
-    "div#player-ads.style-scope.ytd-watch-flexy, div#panels.style-scope.ytd-watch-flexy", // sponsors
-    ".ytd-banner-promo-renderer", ".ytd-video-masthead-ad-v3-renderer", ".ytd-primetime-promo-renderer", // subscribe for premium & youtube tv ads
-    "#masthead-ad", ".ytd-ad-slot-renderer", ".ytd-promoted-sparkles-web-renderer" // others
-  ];
-  pageAds.forEach((ad) => {
-    const elements = document.querySelectorAll(ad);
-    elements.forEach((el) => {
-      if (el) {
-        el.remove();
-      }
-    });
-  });
+const init = async () => {
+  Document.prototype.hideElementsBySelector = (selector) =>
+    [...document.querySelectorAll(selector)].forEach(
+      (el) => (el.style.display = "none")
+    );
+
+    hideAds();
 };
+
+init();
