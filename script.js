eaac3464576c7b66e6b9f7dc45e8f09d71f28ca4
James <86011275+James000916@users.noreply.github.com>
2023-11-10 16:00:36 +0800

    v3.0.1

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const hideAds = async () => {
-  await new Promise((resolve, _reject) => {
+  await new Promise((resolve, reject) => {
     const videoContainer = document.getElementById("movie_player");
 
     const setTimeoutHandler = () => {
       const isAd = videoContainer?.classList.contains("ad-interrupting") || videoContainer?.classList.contains("ad-showing");
-      const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
-      const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
       const videoPlayer = document.getElementsByClassName("video-stream")[0];
+      const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
+      const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
+      const skipButton = document.querySelector(".ytp-ad-skip-button");
+      const skipButtonModern = document.querySelector(".ytp-ad-skip-button-modern");
 
       if (isAd && skipLock) {
         videoPlayer.muted = true;
         videoPlayer.currentTime = videoPlayer.duration - 1;
-        videoPlayer.paused && videoPlayer.play()
-        document.querySelector(".ytp-ad-skip-button")?.click();
-        document.querySelector(".ytp-ad-skip-button-modern")?.click();
+        videoPlayer.paused && videoPlayer.play();
+        skipButton?.click();
+        skipButtonModern?.click();
       } else if (isAd && surveyLock) {
-        document.querySelector(".ytp-ad-skip-button")?.click();
-        document.querySelector(".ytp-ad-skip-button-modern")?.click();
+        skipButton?.click();
+        skipButtonModern?.click();
       }
 
       const staticAds = [
@@ -31,7 +33,7 @@ const hideAds = async () => {
       ];
 
       staticAds.forEach((ad) => {
-        document.hideElementsBySelector(ad);
+        document.removeAdElements(ad);
       });
 
       resolve();
@@ -44,7 +46,7 @@ const hideAds = async () => {
 };
 
 const init = async () => {
-  Document.prototype.hideElementsBySelector = (selector) =>
+  Document.prototype.removeAdElements = (selector) =>
     [...document.querySelectorAll(selector)].forEach((el) => 
       (el.remove())
     );
