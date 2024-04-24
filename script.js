aa3f415b8f2e708c49a6a4f348d5bc1d41e771d0
James <86011275+James000916@users.noreply.github.com>
2024-04-24 14:28:33 +0800

    update

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // Thank you for using MeowBlock!😺
 
 const hideAds = async () => {
-  const response = await fetch('https://james000916.github.io/MeowBlock/variables.json');
-  const variables = await response.json();
-
   await new Promise((resolve, reject) => {
-    const videoContainer = document.getElementById(variables.videoContainer);
+    const videoContainer = document.getElementById("movie_player");
 
     const setTimeoutHandler = () => {
       const isAd = videoContainer?.classList.contains("ad-interrupting") || videoContainer?.classList.contains("ad-showing");
       const videoPlayer = document.getElementsByClassName("video-stream")[0];
       const surveyLock = document.querySelector(".ytp-ad-survey")?.length > 0;
-      const skipLock = document.querySelector(".ytp-ad-preview-text")?.innerText;
+      const skipLock = document.querySelector(".ytp-preview-ad")?.innerText;
       const skipLockModern = document.querySelector(".ytp-ad-preview-text-modern")?.innerText;
-      const skipButton = document.querySelector(".ytp-ad-skip-button");
+      const skipButton = document.querySelector(".ytp-skip-ad-button");
       const skipButtonModern = document.querySelector(".ytp-ad-skip-button-modern");
 
       if ((isAd && skipLock) || (isAd && skipLockModern)) {
@@ -27,7 +24,17 @@ const hideAds = async () => {
         skipButtonModern?.click();
       }
 
-      const staticAds = variables.staticAds;
+      const staticAds = [
+        ".ytd-companion-slot-renderer", ".ytd-action-companion-ad-renderer", // in-feed video ads
+        ".ytd-watch-next-secondary-results-renderer.sparkles-light-cta", ".ytd-unlimited-offer-module-renderer", // similar components
+        ".ytp-ad-overlay-image", ".ytp-ad-text-overlay", // deprecated overlay ads
+        "div#root.style-scope.ytd-display-ad-renderer.yt-simple-endpoint", "div#sparkles-container.style-scope.ytd-promoted-sparkles-web-renderer",
+        ".ytd-display-ad-renderer", ".ytd-statement-banner-renderer", ".ytd-in-feed-ad-layout-renderer", // homepage ads
+        "div#player-ads.style-scope.ytd-watch-flexy, div#panels.style-scope.ytd-watch-flexy", // sponsors
+        ".ytd-banner-promo-renderer", ".ytd-video-masthead-ad-v3-renderer", ".ytd-primetime-promo-renderer", // subscribe for premium & youtube tv ads
+        "#masthead-ad", ".ytd-ad-slot-renderer", ".ytd-promoted-sparkles-web-renderer" // others
+      ];
+
       staticAds.forEach((ad) => {
         document.removeAdElements(ad);
       });
